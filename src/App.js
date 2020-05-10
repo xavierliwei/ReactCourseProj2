28d85bb12ecb95b422e575159bfcfaf8180d1262
Wei Li <wl2451@columbia.edu>
2020-05-10 16:14:18 -0700

    style homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './home';
+import Home from './pages/home/home';
 
 function App() {
   return (
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './home.styles.scss';
+import Directory from '../../components/directory/directory';
+
+const Home = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Home;
